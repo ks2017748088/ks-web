28e2a89fe24a7fc5dcfe91e54470125326fa52e5
ks2017748088 <jny0821@naver.com>
2020-04-14 22:17:14 +0900

    init

--- src/main/java/com/nayoung/app/controller/IndexController.java
+++ src/main/java/com/nayoung/app/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.nayoung.app.Controller;
+package com.nayoung.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
